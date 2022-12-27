140b6599de18cdf752d62a42c75873e72bda8f0f
Ben <ben@thedesigncollaborative.co>
2022-12-27 23:02:15 +0000

    add on_delete directive to migration file for cards

--- priv/repo/migrations/20221217001700_create_cards.exs
+++ priv/repo/migrations/20221217001700_create_cards.exs
@@ -5,7 +5,7 @@ defmodule Muna.Repo.Migrations.CreateCards do
     create table(:cards) do
       add :front, :string
       add :back, :string
-      add :deck_id, references(:decks)
+      add :deck_id, references(:decks, on_delete: :delete_all)
 
       timestamps()
     end
